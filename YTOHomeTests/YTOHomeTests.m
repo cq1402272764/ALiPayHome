ce827bc3aff183fc0081e7aa22e987b3d9e24c71
cq1402272764 <1402272764@qq.com>
2017-06-12 10:22:15 +0800

    高仿支付宝首页代码
    高仿支付宝首页代码

---
+++ YTOHomeTests/YTOHomeTests.m
@@ -0,0 +1,39 @@
+//
+//  YTOHomeTests.m
+//  YTOHomeTests
+//
+//  Created by Qing Chang on 2017/6/7.
+//  Copyright © 2017年 Qing Chang. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface YTOHomeTests : XCTestCase
+
+@end
+
+@implementation YTOHomeTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
